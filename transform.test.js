ddc3b7955fe358580dd8b1ff0ccda4a0dfbd4c91
touhidcodes <touhidcodes@gmail.com>
2025-02-08 23:12:38 +0600

    add: test

--- transform.test.js
+++ transform.test.js
@@ -1,3 +1,11 @@
 import { test, it, expect } from "vitest";
+import { transform } from "./transform";
 
-it();
+it("should return a number if string is provided", () => {
+  const stringNumber = "5";
+
+  const result = transform(stringNumber);
+
+  expect(result).toBeTypeOf("number");
+  expect(isNaN(result)).not.toBe(true);
+});
