9b608fd1a7ebb79d79dd4fbfe0ab0ade7d09e783
touhidcodes <touhidcodes@gmail.com>
2025-02-10 23:50:50 +0600

    add: validation test add

--- validation.test.js
+++ validation.test.js
@@ -1,1 +1,9 @@
 import { it, expect } from "vitest";
+
+it("should return an array of numbers if an array of string numbers are provided", () => {
+  const numbers = ["1", "2"];
+
+  const result = cleanNumbers(numbers);
+
+  expect(result[0]).toBeTypeOf("number");
+});
